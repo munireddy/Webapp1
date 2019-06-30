54e7bc3e2b214a8cd8bf6d89783d5c49babae00c
munireddy <munisekhar.reddy@gmail.com>
2019-06-30 17:56:40 -0400

    Added Int a = 0

--- MyPrg1.java
+++ MyPrg1.java
@@ -1,5 +1,6 @@
 class  MyPrg1{
  public static void main(String args[]){
+     int a = 0;
      System.out.println("Hello World from a git Hub 1");
      System.out.println("Hello World from a git Hub 2");
      System.out.println("12 New change added to the Java program");

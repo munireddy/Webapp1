29e5a0379ae853ffb5beb54bb6401d21a9b31d1c
munireddy <munisekhar.reddy@gmail.com>
2019-06-30 17:09:22 -0400

    Updated Java Program with a fix

--- MyPrg1.java
+++ MyPrg1.java
@@ -2,6 +2,6 @@ class  MyPrg1{
  public static void main(String args[]){
      System.out.println("Hello World from a git Hub 1");
      System.out.println("Hello World from a git Hub 2");
-     System.out.println("1 New change added to the Java program");
+     System.out.println("12 New change added to the Java program");
  }
 }

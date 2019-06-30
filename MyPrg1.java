8795b37fc1238cba54e5fbfda8d81aacec80dc75
munireddy <munisekhar.reddy@gmail.com>
2019-06-30 17:47:29 -0400

    Fixed a bug # 1234

--- MyPrg1.java
+++ MyPrg1.java
@@ -3,5 +3,6 @@ class  MyPrg1{
      System.out.println("Hello World from a git Hub 1");
      System.out.println("Hello World from a git Hub 2");
      System.out.println("12 New change added to the Java program");
+     System.out.println("Change added to trigger a SCM polled job");
  }
 }

36b2e1ece6fe9c1afc45419660a397270f0b4e4f
munireddy <munisekhar.reddy@gmail.com>
2019-06-30 17:58:42 -0400

    Update MyPrg1.java

--- MyPrg1.java
+++ MyPrg1.java
@@ -1,7 +1,7 @@
 class  MyPrg1{
  public static void main(String args[]){
      int a = 0;
-     System.out.println("Hello World from a git Hub 1........"+str(a));
+     System.out.println("Hello World from a git Hub 1........"+a);
      System.out.println("Hello World from a git Hub 2");
      System.out.println("12 New change added to the Java program");
      System.out.println("Change added to trigger a SCM polled job");

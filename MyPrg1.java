9352c4ea0e508e72b81633ca5b2b05231ec74df5
munireddy <munisekhar.reddy@gmail.com>
2019-06-30 17:07:07 -0400

    Update MyPrg1.java

--- MyPrg1.java
+++ MyPrg1.java
@@ -2,5 +2,6 @@ class  MyPrg1{
  public static void main(String args[]){
      System.out.println("Hello World from a git Hub 1");
      System.out.println("Hello World from a git Hub 2");
+     System.out.println(" New change added to the Java program");
  }
 }

b4a21343437436ce0645df1b9f641c1cc98da442
munireddy <munisekhar.reddy@gmail.com>
2019-06-30 18:02:02 -0400

    Update MyPrg1.java

--- MyPrg1.java
+++ MyPrg1.java
@@ -1,6 +1,6 @@
 class  MyPrg1{
  public static void main(String args[]){
-     int a = 0;
+     int a = 100;
      System.out.println("Hello World from a git Hub 1........"+a);
      System.out.println("Hello World from a git Hub 2");
      System.out.println("12 New change added to the Java program");

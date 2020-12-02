d351e2e35dedb4ea344015847564d5d5f40f7a52
munireddy <munisekhar.reddy@gmail.com>
2020-12-01 21:45:49 -0500

    Added a SOP to the program

--- MyPrg1.java
+++ MyPrg1.java
@@ -1,6 +1,7 @@
 class  MyPrg1{
  public static void main(String args[]){
      int a = 100;
+     System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Hello World from a git Hub 0........ 1201 Jenkins Trigger");
      System.out.println("Hello World from a git Hub 1........ 0322 Jenkins Trigger");
      System.out.println("Hello World from a git Hub 1........"+a);
      System.out.println("Hello World from a git Hub 2");
